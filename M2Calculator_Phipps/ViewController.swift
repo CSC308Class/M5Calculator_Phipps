c9d4eeea3fdac7df838c8657a8edbecb0d956203
Makendra <gamerchan756@gmail.com>
2026-01-29 11:54:04 -0500

    M2CalculatorVersion2

--- M2Calculator_Phipps/ViewController.swift
+++ M2Calculator_Phipps/ViewController.swift
@@ -9,6 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var operand1TextField: UITextField!
+    
+    @IBOutlet weak var operand2TextField: UITextField!
+    
+    @IBOutlet weak var selectOperator: UIButton!
+    
+    @IBAction func operatorButton(_ sender: Any) {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let plusAction =  UIAlertAction(title: "+", style: .default){_ in }
+        actionSheet.addAction(plusAction)
+        present(actionSheet, animated: true)
+        let minusAction =  UIAlertAction(title: "-", style: .default){_ in }
+        actionSheet.addAction(minusAction)
+        let multiplyAction =  UIAlertAction(title: "*", style: .default){_ in }
+        actionSheet.addAction(multiplyAction)
+        let divideAction =  UIAlertAction(title: "/", style: .default){_ in }
+        actionSheet.addAction(divideAction)
+        present(actionSheet, animated: true)
+    }
+    
+    
+    @IBOutlet weak var resultLabel: UITextField!
+    
+    @IBAction func resultButton(_ sender: Any) {
+        let op1 = Int(operand1TextField.text!)!
+        let op2 = Int(operand2TextField.text!)!
+        let op = selectOperator.title(for: .normal)
+        
+        if op == "+"{
+            let result = op1 + op2
+            resultLabel.text = "\(result)"
+        }
+        else if op == "-"{
+            let result = op1 - op2
+            resultLabel.text = "\(result)"
+        }
+        else if op == "*"{
+            let result = op1 * op2
+            resultLabel.text = "\(result)"
+        }
+        else if op == "/"{
+            let result = op1 / op2
+            resultLabel.text = "\(result)"
+        }
+        else{
+            print("Invalid Operator")
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -16,4 +68,7 @@ class ViewController: UIViewController {
 
 
 }
+
+//command+B : build your project
+//command+R : run your project
 
